17cf0176effb28b617b31bc89e613997c78a9959
Keye Li <kli32@stevens.edu>
2021-11-20 22:52:51 -0500

    added 2 buttons on ending page

--- cs555-project/pages/ending/index.js
+++ cs555-project/pages/ending/index.js
@@ -3,15 +3,38 @@ import { Text,VStack } from "@chakra-ui/layout"
 import {Layout} from "../../components/core"
 import { QuestionsContext } from "../../components/core/questionsContext/questionsContext"
 import { Button } from "@chakra-ui/button";
-
+import { useRouter } from 'next/dist/client/router';
 
 export default function EndingPage(){
+
+    const router = useRouter();
+
+    function handleNext1(){
+        router.push('./category');
+    }
+
+    function handleNext2(){
+        router.push('./userProfile');
+    }
+
+
     return (
         <Layout >
           <VStack mt='auto' mb='auto'>
               <Text fontSize='2xl'>This is the end</Text>
               <Text fontSize='4xl'>Thank you for using the web</Text>
+              <Button bg='brand.900' size='lg' marginTop = '50px' onClick = {()=>handleNext1()}>
+                Back to Home
+                </Button>
+
+                <Button bg='brand.900' size='lg' marginTop = '50px' onClick = {()=>handleNext2()}>
+                View Profile
+                </Button> 
           </VStack>
+
+          
+
+          
         </Layout>
     )
 }
